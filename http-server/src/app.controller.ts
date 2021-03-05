0913cfc34c5d26d923b239b57c741d618582bbfc
0x4bd0 <abdoumjr@gmail.com>
2021-03-05 11:30:26 +0100

    init http server

--- http-server/src/app.controller.ts
+++ http-server/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MathService } from './math/math.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private mathService : MathService) { }
+  
+private logger = new Logger('AppController')
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('Sum')
+  async sumNumbers(@Body('data') data: Array<number>) {
+
+    this.logger.log('Http request to sum ' + data.toString())
+    
+    return this.mathService.SumNumbers(data)
+    
   }
+
 }
